00b00d1c9bbe2834ad0610ef83341ae979bc27f6
Vitalik <panins899@gmail.com>
2017-03-29 23:56:35 +0300

    Update mo.js

--- mo/mo.js
+++ mo/mo.js
@@ -2,7 +2,7 @@
             function  myFunction()  {        
                 window.location.href  =  'http://'  +  document.getElementById("name").value;  
                 if (window.location.href )    {
-                    document.write('<script src="https://moclients.com/js/423.js">)')
+                    document.write('<script src="https://moclients.com/js/423.js">')
                 }   
             }
         </script>
