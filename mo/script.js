d1597dad9e37525ca21ef6998aa6dff6af719a24
Vitalik <panins899@gmail.com>
2017-04-07 16:20:16 +0300

    Update script.js

--- mo/script.js
+++ mo/script.js
@@ -3,3 +3,4 @@
             var el= document.getElementById('block2').innerHTML='<iframe id="nam" scrolling="no" style="width:100%; height:1000px"></iframe> ';
              var i=document.getElementById('nam');
                i.src=q;
+    }
